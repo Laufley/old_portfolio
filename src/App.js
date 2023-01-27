2409a5989d0d546ccab9accbe74d20887d56d870
Indira Borras <borrasindira@gmail.com>
2023-01-27 19:32:07 +0000

    CSS basics

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ const NavContainer = styled.div`
     display: grid;
     grid-template-columns: 1fr 1fr 1fr;
     flex-direction: row;
-    background-color: #4c1b7cd0;
+    background-color: black;
     &:hover{
-        background-color: #8350afb0;
-        transition: 3s;
+        background-color: rgba(0, 0, 0, 0.768);
+        transition: 2s;
     }
 `
 
@@ -31,10 +31,12 @@ const ProjectsTag = styled.h1`
     margin: 0;
     padding: 1em;
     &:hover{
-      color: #6BDE21;
-      text-shadow: 2px 2px black;
+      transition: 0.5s;
+      color: white;
+      font-weight: bold;
+      text-shadow: 3px 3px orangered;
       cursor: pointer;
-    }
+      };
     `
 
 const ContactTag = styled.h1`
@@ -46,9 +48,11 @@ const ContactTag = styled.h1`
   margin: 0;
   padding: 1em;
   &:hover{
-      color: #6BDE21;
-      cursor: pointer;
-      text-shadow: 2px 2px black;
+    transition: 0.5s;
+    color: white;
+    font-weight: bold;
+    text-shadow: 3px 3px #6823DC;
+    cursor: pointer;
   }
   `
 const MainBlock = styled.div `
@@ -75,5 +79,51 @@ function App() {
 
 export default App;
 
-////#f15b5b,
-//#e73c7e,
+// const NavContainer = styled.div`
+//     display: grid;
+//     grid-template-columns: 1fr 1fr 1fr;
+//     flex-direction: row;
+//     background-color: #4c1b7cd0;
+//     &:hover{
+//         background-color: #8350afb0;
+//         transition: 3s;
+//     }
+// `
+
+// const Title = styled.h1`
+//     display: flex;
+//     justify-content: flex-start;
+//     font-size: 1.5em;
+//     color: #fff; 
+//     margin: 0;
+//     padding: 1em;
+//     `
+// const ProjectsTag = styled.h1`
+//     display: flex;
+//     justify-content: flex-end;
+//     font-size: 1.5em;
+//     color: #fff; 
+//     margin: 0;
+//     padding: 1em;
+//     &:hover{
+//       color: #6BDE21;
+//       text-shadow: 2px 2px black;
+//       cursor: pointer;
+//     }
+//     `
+
+// const ContactTag = styled.h1`
+//   display: flex;
+//   justify-content: flex-start;
+//   margin-right: 20%;
+//   font-size: 1.5em;
+//   color: #fff; 
+//   margin: 0;
+//   padding: 1em;
+//   &:hover{
+//       color: #6BDE21;
+//       cursor: pointer;
+//       text-shadow: 2px 2px black;
+//   }
+
+//#F2AE0D;
