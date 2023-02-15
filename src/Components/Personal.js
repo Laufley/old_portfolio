d6fc9baebacbea134146edb13b3ef1804a381a9a
Indira Borras <borrasindira@gmail.com>
2023-02-15 22:35:01 +0000

    personal page written and duck easter egg on hover done

--- src/Components/Personal.js
+++ src/Components/Personal.js
@@ -1,4 +1,5 @@
 import {React, useState} from 'react'
+import './Personal.css';
 import styled from 'styled-components';
 import Header from '../Components/Header';
 import { Link } from 'react-router-dom';
@@ -90,6 +91,9 @@ const Button = styled.div`
     box-shadow: rgba(240, 46, 170, 0.4) 5px 5px, rgba(240, 46, 170, 0.3) 10px 10px, rgba(240, 46, 170, 0.2) 15px 15px;
     }
 `
+const Text = styled.p `
+font-size:16px;
+`
 
 const Personal = () => {
 
@@ -102,9 +106,9 @@ const Personal = () => {
         <Aside>
             <Picture src={personalImg}></Picture>
             <Details> 
-                    <LinkStyle to='/CV'> <i class="fa">&#xf12e;</i> Résumé </LinkStyle>
-                    <LinkStyle to='https://github.com/Laufley' target="_blank"><i class="fa fa-github"> GitHub</i></LinkStyle>
-                    <LinkStyle to='https://www.linkedin.com/in/indira-borras/' target="_blank"><i class="fa fa-linkedin"> LinkedIn</i></LinkStyle>
+                    <LinkStyle to='/CV'> <i className="fa">&#xf12e;</i> Résumé </LinkStyle>
+                    <LinkStyle to='https://github.com/Laufley' target="_blank"><i className="fa fa-github"> GitHub</i></LinkStyle>
+                    <LinkStyle to='https://www.linkedin.com/in/indira-borras/' target="_blank"><i className="fa fa-linkedin"> LinkedIn</i></LinkStyle>
             </Details>
         </Aside>
         <Main>
@@ -112,70 +116,98 @@ const Personal = () => {
                 and a bit of a goof.</h1></Intro>
             <Content>
             <h2>Life</h2>
-            <pre><p> 
+            <pre><Text> 
             life = list(range(1, 1001)) <br></br>
             print(life)<br></br><br></br>
 
             lemonade = 0<br></br>
             for lemons in life:<br></br>
             lemonade += lemons<br></br>
-            print Lemonade</p>
+            print Lemonade</Text>
             </pre>
-            <h5>What is a Spaniard doing in Edinburgh, Indira?</h5>
-            Good question. I sometimes wonder that myself. I moved here for love, leaving my previous career behind. And you know what? I don't regret it, if there is something out there to fight for, that is love - for cheesy as it sounds. Btw, in the end it didn't work out and we split up after 10y BUT! remember the lemons ;)
+            <section>
+                <h4>What is a Spaniard doing in Edinburgh, Indira?</h4>
+                <Text>Good question. I sometimes wonder that myself. I moved here for love, leaving my previous career behind. And you know what? I don't regret it, if there is something out there to fight for, that is love - for cheesy as it sounds. Btw, in the end it didn't work out and we split up after 10y BUT! remember the lemons ;) </Text>
 
-            <h5>what do you take from your previous working experiences?</h5>
+                <h4>What do you take from your previous work experiences?</h4>
+                <Text>Well, everything! Because I'm a bit greedy haha <br></br><br></br>
+                I'm very grateful for all the experiences I've had the opportunity to have and the amazing people I've crossed paths with. I've kept so many of these connections...<br></br>
+                Never a dull moment, always moving when I got too comfortable so that I could keep improving myself. <br></br><br></br>
+                When I moved countries from Spain leaving behind my career as a psychologyst in the hospital, my main priority (beyond having a basic income) was to learn English, that's why I happily started from sratch on a <b>waitress role</b> and then braved it into <b>customer service.</b><br></br><br></br>
+                Once I started feeling comfortable talking with people all day in English, and my performance at work improved, I didn't hesitate to accept the promotion to the <b>Social Media</b> team which would allow me to work on my English writing skills. <br></br>
+                It was fantastic! A brilliant cooperation among our small team to take care of different social media accounts for our clients. Eventually, it started to feel too predictible (well, as predictible as social media can be) so I focused more on taking on some learning courses, jumping into our processes to improve them, and eventually earning more autonomy on a more leading role within the team where I got to deliver training to the new members and have my first face-to-face interactions with one of our clients. <br></br><br></br>
+                But once I did all of that... What was next for me? There was not much to look up for, really. So I decided to change careers. If I had done it once I could do it again, right?<br></br><br></br>
+                By then, I believed that I couldn't access any good working opportunity because of my trajectory, so I got myself into recruitment/sales. "BUT" - I told myself - "I'll only do it under one self-imposed condition"; if I was to take that crazy jump, at least I wanted to make sure it was on an area I would enjoy, <i>so I chose <b>IT</b> recruitment.</i> There are a few reasons for WHY tech, but that would be a tangent.<br></br><br></br>
+                <b>I've been an IT recruiter for almost 4 years now.</b> From junior to senior. From shy calls to delivering influencial market insights to my clients. And from being anxious at networking events to represent our company on a big tech event sponsored by one of our clients in Stockholm.<br></br>
+                I have really enjoyed my path in IT recruitment, and I still can hardly believe how lucky I've been for having been trusted with the freedon of do things my way. I remember I was the only IT recruiter in <i>Stafffinders</i> by then, yet my manager and the CEO believed in me and supported me into growing a whole new IT department. I had only 6 months of experience by then! But that pressure brought the best of me and I started feeling more comfortable on the role and speaking with clients.<br></br>
+                <i>Smart Hire IT recruitment Nordics</i> has been, however, my growing pot and free canvass. I joined the startup when we were only 4 in the team and I joined because we shared the same vision about recruiters<i>"I don't like recruiters, they are so cold. No wonder developers feel the same. I want to change the recruitment market! I want to work on a company that nurtures connections and spends the right amount of time on each different profile! I want our company to be different!" - Indira 2021. </i><br></br>My boss had the same vision, everyone on the team, really. And that's how we drove success while wholeheartedly enjoying what we did and how we did it. There are so many things I can say about this team... but that would take pages.<br></br> So yes, I became a senior recruiter in this company, and I took more responsabilities than were expected because I believed in what we were doing and I wanted to improve the way we did it. so I:<br></br>
+                - Led a Social Media audit with the CEO - Which they had never done before. <br></br>
+                - Joined forces with the CEO and together we created a new logo, rebranded the whole company, and created the most flashy website (thanks to the help of a developer): We are very proud of it and the reactions were fantastic.<br></br>
+                - Became account manager of multiple accounts in EU: Succesful partnerships based on care and brutal-yet-carefuly-delivered-honesty when needed.<br></br>
+                - Improved operations within the comany: Writing guides and templates for everyone to use.<br></br>
+                - Brought in new techniques and approaches that are still in use because they work!<br></br>
+                - Training, leading, mentoring new people... I love powerpoint and creating stuff for others.<br></br>
+                <b>I was about to be promoted to <i>"Head of the creative deparment"</i></b> but I had reached a plateu already. I had achieved everything I expected from myself, and the promotion would only imply a change on salary and job title but my work would have been the same, in essence, of what I was already doing. And here is when the tilting moment comes.<br></br><br></br>
+                I had already been in touch with developers daily for 4 years. Sourcing, interviewing, socialising... and what they did trully amazed me. It sounded so exciting! So strage! So full of possibilities! An endless ocean of knowledge-beads. Slowly, over the years, I was getting more and more into the idea of programming but I would never take a jump without planning it well. It never seemed to be the right moment and then... <b>BOOM!! codeBEAM conference 2022, Stockholm.</b><br></br>
+                Basically, a succesful partnership with one of my clients led to the invitation to a BIG tech event they sponsor every year. And when I arrived, and spoke with everyone about their projects, about their goals, and attended all the conferences and panels I was left astounded. So. Many. Interesting. Topics! So much innovation going on. So many bright ideas, so many minds buzzing... and I felt part of it. I really felt comfortable in that environment. WHY DID IT TAKE ME SO MANY YEARS TO FIND THIS OUT?<br></br><br></br>
 
-            
-                
-            {/* <p> <b>Psychology:</b> My journey started in the sunny lands of Barcelona when I worked as a junior psyhoclogyst and research associate at Hospital Universitari de Bellvitge. I led my own neuropsichological evaluations, and participated on different treatments for a variety of mental disorders on top of the research we were carrying. But the universe works in misterious ways and I had to chose between my career or moving to another country for love. I chose love. <br></br><br></br>
-            <b>Waitress and Customer Service:</b> I had to start from scratch again since pursuing a doctorate here wouldn't have brough enough economical safety. I barely spoke any english, but having client-facing roles pushed me to learn the language fast. It was the perfect challenge for my learning needs. And within 3 months of customer service I was promoted to the new Social Media team they had. It was fantastic, because I was already starting to feel slightly comfortable speaking English daily<br></br><br></br>
-            <b>Social Media Community Coordinator:</b>My team and I worked for 3 different clients, making sure their different social media business accounts were thriving. I got to practice a lot of writing english while speaking it daily in the office. It was ideal. Yet again, I started to feel comfortable so I began to innovate and take more leading roles to help the team. That's how in the end I got my first face to face client interactions and experience training new teammates.<br></br><br></br>
-            <b>IT recruitment:</b>The recruitment industry is savage, and that's fine, I don't mind it. Challenges makes you grow. I found it too cold and impersonal though, and that's not how I am. I am a psychologyst! I care about people! So that was me with a new career goal: To change the IT recruiting industry by making it more human. And even when it sounds impossible, I did it! at small scale, at company-scale, but for me, that counts. Specially on my last job where I had more influence on the culture and client handling. Candidates also appreciated it. And that's something I will forever take with me. <br></br>
-            Hard work takes you places, and I finally became a senior recruiter which I so much pursued. Beyond my normal recruter duties headhunting, screening and interviewing candidates, I became the account manager of several clients in Norway, Sweden and Lithuania, provided training for new team members, mentorship to the team and - quite beyond my expected duties:
-            <ul>
-                <li>Improved internal operations and created guides and templates - which keep growing with new team inputs</li>
-                <li>Drove a Social Media audit for the CEO - which they had never done before!</li>
-                <li>Created a new Logo in parnership with the CEO - There is a funny story about that</li>
-                <li>Collaborating with the CEO, together we rebranded the whole company resulting in a defined brand persona, tone of voice, etc. and a flashy website that a Front-End developer helped us create. The reactions from our audience were an absolute success!</li>
-                <li>Client relationships were my forte. We were so succesful that partners were inviting us to their main offices, and one of them even invited me to the codeBEAM conference in Stokcholm they were sponsoring in May 2022. <b>THAT was a life-changing experience for me and the final push to decide become a programmer.</b></li>
-                <li>I became a core member of the team, participated in important decision-making, and they are currently carrying one one of my latest suggestions for expansion - They are doing amazingly so far</li>
-                <li>Got to be promoted to "Head of the creative division" for the new deparment - which is great! BUT. there is always a but. I had already achieved what I wanted to achieve and more. A change in title was only that, a change in title and salary, but my job would be essentially the same. I had reached a career plateau and I am pretty much a hands-on person, I like to be busy, create things and improve things.<br></br>
-                And that's what drove me to studying Software Development. 
-                </li>
-            </ul> */}
-            {/* </p> */}
+                So here I am. Yet again, changing careers. But this time, with endless learning opportunities.<br></br>
+                And here is where I belong.
 
-            <h2>Hobbies</h2>
-            Life is too short and has too much to offer. <br></br>
-            It's quite humbling to think about all the things awaiting for us to discover.<br></br>
-            And I want to learn as much as I can. Maybe that's why I have so many hobbies!
-            <ul>
-                <li><b>Writing:</b> I've been writing collaboratively with people all over the globe for years!</li>
-                <li><b>Reading:</b> Hard sci-fi and Hard fantasy prefered</li>
-                <li><b>Miniature painting:</b> We use them in our team's D&D camapigns</li>
-                <li><b>Boardgames and videogames:</b> Platform eclectic</li>
-                <li><b>Piano</b> just starting</li>
-                <li><b>Video editing:</b> Blender is so complex!</li>
-                <li><b>Documentaries:</b> About science and nature</li>
-                <li><b>Hiking:</b> I belong to nature. I'm a mountain goat</li>
-                <li><b>Travelling:</b> Discovering about the world and other cultures enrichens our scope</li>
-                <li><b>Improving and fixing things:</b> I can't help it.</li>
-            </ul>
-            <h2>Causes I care about</h2>
-            <ul>
-                <li>Sustainability: I've currently adopted/sponsored turtles in the Antartic waters, Corals and SuperCorals, and bee hives</li>
-                <li>Equality of human ressources distribution around the globe: A big ongoing project of mine to get more involved the future to help communities with better access to education</li>
-                <li>EDUCATION: Quality education. We need better scientific and moral standards.</li>
-                <li>Politics: Like it or not, they are key to understand the world we are ruling a bit better.</li>
-                <li>SpaceStuff: I have always been addicted to space topics and our insignificant part in its vastness. The Cosmic Landscape was an eyes-opening book for me.</li>
-            </ul>
-            
-            <h2>I love ducks</h2>
-            <p>
-
-            
-            </p>
+                </Text>
+            </section>
+            <section>
+                <h2>Hobbies</h2>
+                <Text>
+                Life is too short and has too much to offer. <br></br>
+                It's quite humbling to think about all the things awaiting for us to discover.<br></br>
+                And I want to learn as much as I can. Maybe that's why I have so many hobbies! 
+                </Text>
+                <ul>
+                    <Text>
+                    <li><b>Writing:</b> I've been writing collaboratively with people all over the globe for years!</li>
+                    <li><b>Reading:</b> Hard sci-fi and Hard fantasy prefered</li>
+                    <li><b>Miniature painting:</b> We use them in our team's D&D camapigns</li>
+                    <li>Roleplaying like D&D or GURPS: You would be amazed of how much you can grow and learn from it. Very collaborative too ;)</li>
+                    <li><b>Boardgames and videogames:</b> Platform eclectic</li>
+                    <li><b>Piano</b> just starting but one of my songs made it into an indie game!</li>
+                    <li><b>Video editing:</b> Blender is so complex!</li>
+                    <li><b>Documentaries:</b> About science, nature and space</li>
+                    <li><b>Hiking:</b> I belong to nature. I'm a mountain goat</li>
+                    <li><b>Travelling:</b> Discovering about the world and other cultures enrichens your soul</li>
+                    <li><b>Improving and fixing things:</b> I can't help it.</li>
+                    </Text>
+                </ul>
+            </section> 
+            <section>
+                <h2>Causes I care about</h2>
+                <ul><Text>
+                    <li>
+                        <b>Sustainability:</b> 
+                        I've currently adopted turtles in the Antartic, corals in Oceania, trees, and bee hives.<br></br> 
+                        Some links for the curious: 
+                        <a href='https://coralgardeners.org/?gclid=Cj0KCQiAorKfBhC0ARIsAHDzsltT9yk3irj3A4zYIL7wTQ23F5pQOGthgfFGkn8cTWUnBSO2gzywKuQaAp7SEALw_wcB' style={{color: 'aliceblue', textDecoration:'none'}}> CoralGardeners</a>, <a href='https://support.wwf.org.uk/adopt-an-animal' style={{color: 'aliceblue', textDecoration:'none'}}> WWF animals</a>
+                    </li>
+                    <li><b>Children in need</b></li>
+                    <li><b>Education and equality of ressources across the globe</b> A big ongoing project of mine is to get more involved in the future in initiatives that help communities get better access to education</li>
+                    </Text>
+                </ul>
+            </section>
+            <section>
+            <h3>And...</h3>
+                <p className='duck-name'>
+                    <span className="i-letter">I </span>
+                    <span className="l-letter">L</span>
+                    <span className="o-letter">O</span>
+                    <span className="v-letter">V</span>
+                    <span className="e-letter">E </span>
+                    <span className="d-letter">D</span>
+                    <span className="u-letter">U</span>
+                    <span className="c-letter">C</span>
+                    <span className="k-letter">K</span>
+                    <span className="s-letter">S</span>
+                </p>
+            </section>
             </Content>
         </Main>
         </div>}
